e799d65db05f91230aa8712fbe999d80d9e3ab26
Divine-favour <divinefavourdamilare2019@gmail.com>
2022-11-29 22:39:22 +0100

    Fix the subtration sign problem

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = int(str(number)[-1])
+if number < 0:
+    last_digit = last_digit * -1
+else:
+    last_digit = last_digit * 1
 if last_digit > 5:
     print("Last digit of {} is {} and is greater than 5"
           .format(number, last_digit))
