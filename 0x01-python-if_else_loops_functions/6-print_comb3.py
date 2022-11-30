222bc24f0c9ba6a54220c33f66054528f06f6fcd
Divine-favour <divinefavourdamilare2019@gmail.com>
2022-11-30 22:39:29 +0100

    printing the combination of two numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+for i in range(0, 9):
+    for c in range(i+1, 10):
+        if i != 8:
+            if c != i:
+                print("{}{}, ".format(i, c), end="")
+        else:
+            print("{}{}".format(i, c))
