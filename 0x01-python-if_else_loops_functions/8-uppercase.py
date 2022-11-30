7d537bf1978b2e135111c20298d6bfe4c8c930f6
Divine-favour <divinefavourdamilare2019@gmail.com>
2022-11-30 23:51:05 +0100

    lower case letters to upper case

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def uppercase(str):
+    diff = ord('a') - ord('A')
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            c = chr(ord(c) - diff)
+        print("{}".format(c), end="")
+    print("")
