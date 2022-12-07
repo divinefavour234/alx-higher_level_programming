559dcd1fd0675cd5281a2234b24e0f3d43785294
Divine-favour <divinefavourdamilare2019@gmail.com>
2022-12-07 12:32:13 +0100

    simple calculator

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import calculator_1
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    addition = calculator_1.add(a, b)
+    subtraction = calculator_1.sub(a, b)
+    multiplication = calculator_1.mul(a, b)
+    division = calculator_1.div(a, b)
+
+    print("{} + {} = {}".format(a, b, addition))
+    print("{} - {} = {}".format(a, b, subtraction))
+    print("{} * {} = {}".format(a, b, multiplication))
+    print("{} / {} = {}".format(a, b, division))
