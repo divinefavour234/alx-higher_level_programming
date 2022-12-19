3274fa4dc069b3330d7ac82ffaf3057fc37db22f
Divine-favour <divinefavourdamilare2019@gmail.com>
2022-12-19 10:15:48 +0100

    Fix errors

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,5 @@
 def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list) - 1:
         return None
-    for i in range(0, (len(my_list) - 1)):
-        if i == idx:
-            return my_list[i]
+    for idx in range(0, (len(my_list) - 1):
+            return my_list[idx]
