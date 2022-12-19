6fa51f17a3c9e300ffec49f5fdaffce4d218c227
Divine-favour <divinefavourdamilare2019@gmail.com>
2022-12-19 10:07:19 +0100

    Fix errors

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list) - 1:
         return None
-    for i in range(0, (len(my_list) - 1)):
+    if my_list is not None:
+        for i in range(0, (len(my_list) - 1)):
         if i == idx:
             return my_list[i]
