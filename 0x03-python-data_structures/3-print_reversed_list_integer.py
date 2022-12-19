79eba40ff325f3dcbc878c6610762bfb9f7320ee
Divine-favour <divinefavourdamilare2019@gmail.com>
2022-12-19 09:57:45 +0100

    fix pycodestyle

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list is != None:
+    if my_list is not None:
         length = len(my_list)
         length = length - 1
         if length != -1:
