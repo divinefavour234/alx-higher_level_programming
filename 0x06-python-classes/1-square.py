ca5fd7d4b47f27b442fcd449d7e2bbf0ae8d14d3
Divine-favour <divinefavourdamilare2019@gmail.com>
2023-01-03 20:26:18 +0100

    a class Square that defines a square with an attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This Module contain a class
+"""
+
+
+class Square:
+    """
+    This class contains a function that  defines a square
+    """
+
+    def __init__(self, size):
+        """
+        Args:
+            param1: The size of the square
+        """
+        self._size = size
