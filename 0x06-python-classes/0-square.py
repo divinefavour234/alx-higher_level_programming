e09ae06f84b291cc175c6714ccfcc8f98c61968e
Divine-favour <divinefavourdamilare2019@gmail.com>
2023-01-03 20:10:52 +0100

    Created an empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+
+
+"""
+This is a module with some useful functions.
+
+The functions in this module can be used
+"""
+
+
 class Square:
+    """
+    This contains An empty Class
+    """
     # An empty class
     pass
